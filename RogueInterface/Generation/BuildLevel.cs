6aa023e7362782de7a562bbd61d281747044ab90
Krishunter1 <37395705+Krishunter1@users.noreply.github.com>
2021-03-16 11:13:31 +0000

    Part implementation of the generating of the map
    Variables and aspects of the builder pattern have been renamed and changed. A new interface for the generating of the map was created and a new class implementing the interface BuildLevel is created. In build level for now, you can draw out the rooms of the level

---
+++ RogueInterface/Generation/BuildLevel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace RogueInterface.Generation
+{
+    public class BuildLevel : IGenerateMap
+    {
+       /* public void connectRoomWithCorridor()
+        {
+            throw new NotImplementedException();
+        } */
+
+        public void drawRoom(int heightOfRoom, int widthOfRoom)
+        {
+            string printWidth = "|";
+            string printHeight = "";
+            int height = heightOfRoom;
+            int width = widthOfRoom;
+
+            for(int i = 0; i < width; i++)
+            {
+                printWidth = printWidth + "-";
+            }
+            Console.WriteLine(printWidth);
+
+            printHeight = printHeight + "{0, width-1}" + "|";
+            for (int i = 0; i < height; i++)
+            {
+                Console.WriteLine();
+            }
+            Console.WriteLine(printWidth);
+        }
+
+      /*  public void selectRoom(string roomSize)
+        {
+            throw new NotImplementedException();
+        }*/
+    }
+}
--- RogueInterface/Program.cs
+++ RogueInterface/Program.cs
@@ -6,28 +6,62 @@ namespace RogueInterface
     {
         static void Main(string[] args)
         {
-            MapBuilder map = new MapBuilder(0, null, 0, null);
-            
-            for(int i = 0; i < 4;){
+            MapBuilder map = new MapBuilder(0, null, 0, 0, 0, 0, 0);
+
+            Console.WriteLine("How many rooms do you want to generate between 1 to 4 in level 1");
+            int roomInput = Convert.ToInt32(Console.ReadLine());
+            map.NumberOfRooms(roomInput);
+
+            Console.WriteLine("What size of rooms do you want to be generated for levels 1 (small, medium, large)");
+            string sizeOfRooms = Console.ReadLine();
+            map.SizeOfRooms(sizeOfRooms);
+            if (sizeOfRooms == "small")
+            {
+                map.DoorAttachedToRooms(2);
+                map.HeightOfRoom(7);
+                map.WidthOfRoom(7);
+            }
+            else if (sizeOfRooms == "medium")
+            {
+                map.DoorAttachedToRooms(3);
+                map.HeightOfRoom(12);
+                map.WidthOfRoom(12);
+            }
+            else
+            {
+                map.DoorAttachedToRooms(4);
+                map.HeightOfRoom(17);
+                map.WidthOfRoom(17);
+            }
+            map.GameLevel(1);
+            map.buildRooms();
+
+            int heightOfRoom = map.getHeightOfRoom();
+            int widthOfRoom = map.getWidthOfRoom();
+
+           // drawRoom(heightOfRoom, widthOfRoom);
+
+            //commenting out for now the old implementation of main method, will be removed later
+            /*for(int i = 0; i < 4;){
                 if(i==0){
                     Console.WriteLine("How many rooms do you want to generate between 1 to 5? for levels 1 - 10?");
                     int roomInput = Convert.ToInt32(Console.ReadLine());
-                    map.RoomNumber(roomInput);
+                    map.NumberOfRooms(roomInput);
 
                     Console.WriteLine("What size of rooms do you want to be generated for levels 1 - 10? (small, medium, large) ");
-                    string roomSize = Console.ReadLine();
-                    map.RoomSize(roomSize);
-                    if(roomSize == "small"){
-                        map.DoorNumber(2);
+                    string sizeOfRooms = Console.ReadLine();
+                    map.SizeOfRooms(sizeOfRooms);
+                    if(sizeOfRooms == "small"){
+                        map.DoorAttachedToRooms(2);
                     }
-                    else if(roomSize == "medium"){
-                        map.DoorNumber(3);
+                    else if(sizeOfRooms == "medium"){
+                        map.DoorAttachedToRooms(3);
                     }
                      else
                     {
-                        map.DoorNumber(4);
+                        map.DoorAttachedToRooms(4);
                     }
-                    map.LevelRange("1- 10");
+                    map.RangeOfLevel("1- 10");
                     map.buildRooms();
                     i++;
                 }
@@ -35,22 +69,22 @@ namespace RogueInterface
                 else if(i==1){
                     Console.WriteLine("How many rooms do you want to generate between 1 to 5? for levels 11 - 20?");
                     int roomInput = Convert.ToInt32(Console.ReadLine());
-                    map.RoomNumber(roomInput);
+                    map.NumberOfRooms(roomInput);
 
                     Console.WriteLine("What size of rooms do you want to be generated for levels 11 - 20? (small, medium, large) ");
                     string roomSize = Console.ReadLine();
-                    map.RoomSize(roomSize);
+                    map.SizeOfRooms(roomSize);
                     if(roomSize == "small"){
-                        map.DoorNumber(2);
+                        map.DoorAttachedToRooms(2);
                     }
                     else if(roomSize == "medium"){
-                        map.DoorNumber(3);
+                        map.DoorAttachedToRooms(3);
                     }
                     else
                     {
-                         map.DoorNumber(4);
+                         map.DoorAttachedToRooms(4);
                     }
-                    map.LevelRange("11 - 20");
+                    map.RangeOfLevel("11 - 20");
                     map.buildRooms();
                     i++;
                 }
@@ -58,22 +92,22 @@ namespace RogueInterface
                 else if(i==2){
                     Console.WriteLine("How many rooms do you want to generate between 1 to 5? for levels 21 - 30?");
                     int roomInput = Convert.ToInt32(Console.ReadLine());
-                    map.RoomNumber(roomInput);
+                    map.NumberOfRooms(roomInput);
 
                     Console.WriteLine("What size of rooms do you want to be generated for levels 21 - 30? (small, medium, large) ");
                     string roomSize = Console.ReadLine();
-                    map.RoomSize(roomSize);
+                    map.SizeOfRooms(roomSize);
                     if(roomSize == "small"){
-                        map.DoorNumber(2);
+                        map.DoorAttachedToRooms(2);
                     }
                     else if(roomSize == "medium"){
-                     map.DoorNumber(3);
+                     map.DoorAttachedToRooms(3);
                     }
                     else
                     {
-                        map.DoorNumber(4);
+                        map.DoorAttachedToRooms(4);
                     }
-                    map.LevelRange("21 - 30");
+                    map.RangeOfLevel("21 - 30");
                     map.buildRooms();
                     i++;
                 }
@@ -81,26 +115,26 @@ namespace RogueInterface
                 else if(i==3){
                     Console.WriteLine("How many rooms do you want to generate between 1 to 5? for levels 31 - 40?");
                     int roomInput = Convert.ToInt32(Console.ReadLine());
-                    map.RoomNumber(roomInput);
+                    map.NumberOfRooms(roomInput);
 
                     Console.WriteLine("What size of rooms do you want to be generated for levels 31 - 40? (small, medium, large) ");
                     string roomSize = Console.ReadLine();
-                    map.RoomSize(roomSize);
+                    map.SizeOfRooms(roomSize);
                     if(roomSize == "small"){
-                        map.DoorNumber(2);
+                        map.DoorAttachedToRooms(2);
                     }
                     else if(roomSize == "medium"){
-                        map.DoorNumber(3);
+                        map.DoorAttachedToRooms(3);
                     }
                     else
                     {
-                        map.DoorNumber(4);
+                        map.DoorAttachedToRooms(4);
                     }
-                    map.LevelRange("31 - 40");
+                    map.RangeOfLevel("31 - 40");
                     map.buildRooms();
                     i++;
                 }
-            }
+            }*/
         }
     }
 }
