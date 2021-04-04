b57a723010ad5bc0ba34869f7a7e0d404cf69763
Krishunter1 <37395705+Krishunter1@users.noreply.github.com>
2021-04-04 20:13:12 +0100

    Setting up the first steps of the Interceptor Pattern. An Context Object Interface is set up for the Events and the Interceptor for it. The EventDispatcher has been created but needs to be filled in.

---
+++ RogueInterface/Interfaces/IEventInterceptor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+//A simple Interceptor interface set up, to allow the client to change the remote object.
+namespace RogueInterface.Generation
+{
+    interface IEventInterceptor
+    {
+        public void OnEventGenerationRequest(IEventParamaters context);
+    }
+}
