94747bd9f83b6111a3dcc5adcfe1ffe7fb40d08a
puritylake <robertoshea2k11@gmail.com>
2021-04-19 20:47:00 +0100

    Add testing project

---
+++ RogueInterfaceTests/UnitTest1.cs
@@ -0,0 +1,20 @@
+using NUnit.Framework;
+
+using RogueInterface.Generation;
+
+namespace RogueInterfaceTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
